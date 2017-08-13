1551719de8bd9984ab659d9fa4c8aef7e3638d6a
Kelly Copley <copleykj@gmail.com>
2017-08-13 15:27:51 -0400

    user getCollectionForParentLink instead of old api

--- server/server.js
+++ server/server.js
@@ -16,13 +16,13 @@ CommentsCollection.allow({
 
 CommentsCollection.after.insert(function afterInsert(userId, comment) {
     // when a comment is added, update the comment count for the object being commented on
-    const collection = LinkableModel.getCollectionForRegisteredType(comment.objectType);
+    const collection = this.transform().getCollectionForParentLink();
     collection && collection.update(comment.linkedObjectId, { $inc: { _commentCount: 1 } });
 });
 
 CommentsCollection.after.remove(function afterRemove(userId, comment) {
     // when a comment is deleted, update the comment count for the object being commented on
-    const collection = LinkableModel.getCollectionForRegisteredType(comment.objectType);
+    const collection = this.transform().getCollectionForParentLink();
     collection && collection.update(comment.linkedObjectId, { $inc: { _commentCount: -1 } });
 
     // if there are any likes or comments for the deleted comment, delete them
