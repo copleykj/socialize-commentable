e1eb6dc19fd446e8f51c51a81ac481de03d88a95
StorytellerCZ <storyteller@freedombase.net>
2018-03-14 11:07:39 +0100

    Update likeable dependency

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 /* global Package */
 Package.describe({
-    name: 'socialize:commentable',
-    summary: 'A package for implementing social commenting',
-    version: '1.0.0',
-    git: 'https://github.com/copleykj/socialize-commentable.git',
+  name: 'socialize:commentable',
+  summary: 'A package for implementing social commenting',
+  version: '1.0.1',
+  git: 'https://github.com/copleykj/socialize-commentable.git'
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.3');
+  api.versionsFrom('1.3');
 
-    api.use('socialize:likeable@1.0.0');
+  api.use('socialize:likeable@1.0.2');
 
-    api.imply('socialize:likeable');
+  api.imply('socialize:likeable');
 
-    api.mainModule('server/server.js', 'server');
-    api.mainModule('common/common.js');
+  api.mainModule('server/server.js', 'server');
+  api.mainModule('common/common.js');
 });
