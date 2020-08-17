b409b3a6464f801cfa4917f2e5f9fba2cc4d4d65
Kelly Copley <copleykj@gmail.com>
2020-08-17 09:13:20 -0400

    fix publishing node_modules

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:commentable',
     summary: 'A package for implementing social commenting',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/copleykj/socialize-commentable.git',
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.10.2');
 
     api.use([
-        'socialize:likeable@1.0.3',
+        'socialize:likeable@1.0.4',
         'reywood:publish-composite@1.7.3',
     ]);
 
